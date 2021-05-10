1f6d748e0fd4047276a5034a962fd6465a715a19
Kristine <kreaselee@gmail.com>
2021-05-09 23:00:05 -0700

    Updates

--- app/src/androidTest/java/com/example/finalproject/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/finalproject/ExampleInstrumentedTest.java
@@ -1,14 +1,19 @@
 package com.example.finalproject;
 
+import android.content.ComponentName;
 import android.content.Context;
 
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +22,12 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.finalproject", appContext.getPackageName());
     }
+
 }
